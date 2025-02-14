46e98306fcbbc5d864e6e54ac9564acbd5b084fd
Engineer-Collo <collinslikhomba@gmail.com>
2025-02-14 11:16:46 +0300

    working on sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+
+    if length == 1:
+        print([0])
+        return
+    #initialize my_fibonacci
+    my_fibonacci = [0,1] 
+
+    #loop through my_fibonacci
+    while len(my_fibonacci) < length:
+      my_fibonacci.append(my_fibonacci[-2] + my_fibonacci[-1])
+
+    print(my_fibonacci)
+
+print(print_fibonacci(10))
